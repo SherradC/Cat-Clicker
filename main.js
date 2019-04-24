b277b2b3010b76f0ee49f56806b163fe77349609
Sherrad Cosby <sherrad.c@gmail.com>
2019-04-24 15:50:20 -0400

    add cat fix

--- main.js
+++ main.js
@@ -13,62 +13,144 @@ var model={
         new Cat("Nipsey", "images/2cats.jpg"),
         new Cat("Hussle", "images/lcat.jpg"),
         new Cat("Pac", "images/blackCat.jpg")
-    ]
-}
+    ],
 
-
-var octopus = {
-    getCats: function(){
-        return model.cats;
+    addCat: function(catName, catPicture) {
+    const cat = {
+      name: catName,
+      picture: catPicture,
+      clicks: 0
     }
+    model.cats.push(cat);
+  }
 }
 
 
+
+// Middle part
+var octopus = {
+  getCats: function(){
+    return model.cats;
+  },
+  addCat: function(catName, catPicture) {
+    model.addCat(catName, catPicture);
+  }
+}
+
+// View part
 var view = {
-    init: function(){
-        // select left column in dom
-        const leftColumn = document.querySelector(".col-4");
+  // Initialize the left cat list
+  init: function() {
+    // select left column in dom
+    const leftColumn = document.querySelector(".col-4");
+    //reset left column
+    leftColumn.innerHTML = "";
+    
+    // Construct cat list
+    const ul = document.createElement("ul");
+    ul.className = "list-group";
+    for(let cat of octopus.getCats()) {
+      const li = document.createElement("li");
+      li.className = "list-group-item";
+      li.textContent = cat.name;
+      li.addEventListener("click", ()=>{
+          this.render(cat);
+      })
+      ul.appendChild(li);
+    }
+    leftColumn.appendChild(ul);
 
-        // construct list
-        const ul = document.createElement("ul");
-        ul.className = "list-group text-center"
-        for(let cat of octopus.getCats()){
-            const li = document.createElement("li");
-            li.className = "list-group-item"
-            li.textContent = cat.name;
-            li.addEventListener("click", ()=>{
-                this.render(cat);
-            })
+    // add an admin button
+    const adminBtn = document.createElement("button");
+    adminBtn.textContent = "Add New Cat";
+    adminBtn.className = "btn btn-warning mt-3";
+    adminBtn.addEventListener("click", this.showForm);
+    leftColumn.appendChild(adminBtn);
 
+    // Add an empty form
+    const form = document.createElement("form");
+    form.addEventListener("submit", this.addCat);
+    leftColumn.appendChild(form);
+  },
+  // render the cat on the right
+  render: function(cat) {
+    const rightColumn = document.querySelector(".col-8");
+    // Clear the display area
+    rightColumn.innerHTML = "";
+    // display the current cat
+    const h1 = document.createElement("h1");
+    h1.textContent = cat.name;
+    rightColumn.appendChild(h1);
+    
+    const img = document.createElement("img");
+    img.setAttribute("src", cat.picture);
+    img.setAttribute("alt", "cat");
+    img.addEventListener("click", ()=>{
+      cat.clicks++; 
+      h2.textContent = `Clicks: ${cat.clicks}`;
+    })
+    rightColumn.appendChild(img);
 
-            ul.appendChild(li);
-        }
+    const h2 = document.createElement("h2");
+    h2.textContent = `Clicks: ${cat.clicks}`;
+    // h2.textContent = "Clicks: " + cat.clicks; 
+    rightColumn.appendChild(h2);
+  },
 
-        leftColumn.appendChild(ul);
-    },
+  
+  showForm: function() {
+    const form = document.querySelector("form");
+    // reset the form
+    form.innerHTML = "";
+    // Adding name input 
+    const nameGroup = document.createElement("div");
+    nameGroup.className = "form-group";
+    const nameLabel = document.createElement("label");
+    nameLabel.textContent = "Cat Name";
+    nameLabel.setAttribute("for", "catName");
+    const nameInput = document.createElement("input");
+    nameInput.className = "form-control";
+    nameInput.setAttribute("id", "catName");
+    nameGroup.appendChild(nameLabel);
+    nameGroup.appendChild(nameInput);
+    form.appendChild(nameGroup);
 
-    render: function(cat){
-        const rightColumn= document.querySelector(".col-8");
-            // clear the display area
-        rightColumn.innerHTML = "";
+    // Adding picture input
+    const imgGroup = document.createElement("div");
+    imgGroup.className = "form-group";
+    const imgLabel = document.createElement("label");
+    imgLabel.textContent = "Cat Picture";
+    imgLabel.setAttribute("for", "catPicture");
+    const imgInput = document.createElement("input");
+    imgInput.className = "form-control";
+    imgInput.setAttribute("id", "catPicture");
+    imgGroup.appendChild(imgLabel);
+    imgGroup.appendChild(imgInput);
+    form.appendChild(imgGroup);
 
-        const h1 = document.createElement("h1");
-            h1.textContent = cat.name;
-        rightColumn.appendChild(h1);
+    // Adding buttons
+    const submitBtn = document.createElement("button");
+    submitBtn.textContent = "Submit";
+    submitBtn.className = "btn btn-success";
+    form.appendChild(submitBtn);
+    
+    const cancelBtn = document.createElement("button");
+    cancelBtn.textContent = "Cancel";
+    cancelBtn.className = "btn btn-danger float-right"; 
+    cancelBtn.setAttribute("type", "button");
+    cancelBtn.addEventListener("click", ()=> {
+      form.innerHTML="";
+    })
+    form.appendChild(cancelBtn);
+  },
 
-        const img = document.createElement("img");
-            img.setAttribute("src", cat.picture);
-            img.setAttribute("alt", "Big Cat");
-            img.addEventListener("click", ()=>{
-                cat.clicks++;
-            h2.textContent = `Clicks: ${cat.clicks}`;
-            })
-        rightColumn.appendChild(img);
-
-        const h2 = document.createElement("h2");
-            h2.textContent = `Clicks: ${cat.clicks}`;
-        rightColumn.appendChild(h2);
-        }
-        }
-
+  addCat: function(e) {
+    e.preventDefault();
+    const catName = document.querySelector("#catName").value;
+    const catPicture = document.querySelector("#catPicture").value;
+    octopus.addCat(catName, catPicture);
     view.init();
+  }
+}
+
+view.init();
